13262f0be219484e9d0d210227e1c48e1d6a82c6
ssrout <smruti.sambit.rout@gmail.com>
2020-08-31 15:34:06 +0530

    ctci c# version solution

---
+++ CTCI/Csharp/CTCI_6th_Edition/CTCI_6th_Edition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CTCI_6th_Edition.Arrays_and_Strings;
+
+namespace CTCI_6th_Edition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------1.1-----------");
+            isUnique isunique=new isUnique();
+            isunique.Result();
+            Console.WriteLine("----------1.2-----------");
+            Check_Permutation check_Permutation=new Check_Permutation();
+            check_Permutation.Result();
+            Console.WriteLine("----------1.3-----------");
+            URLify url=new URLify();
+            url.Result();
+        }
+    }
+}
