643d16f6c8b02773a0beca8109b3a24ccb9464ff
ssrout <smruti.sambit.rout@gmail.com>
2020-09-01 02:00:15 +0530

    p1 added

---
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Linked Lists/LinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpSolution.Linked_Lists
+{
+    /*
+     * Plain Linked list with 
+     * Prepend
+     * Append
+     * delete node
+    */
+
+    internal class Node
+    {
+        internal int data;
+        internal Node next;
+        public Node(int d)
+        {
+            next = null;
+            data = d;
+        }
+    }
+    public class LinkedList
+    {
+        internal Node head;
+        int count;
+        public LinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        internal void Prepend(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            count++;
+        }
+
+        internal void Append(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                count++;
+                return;
+            }
+            Node currentNode = head;
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = new Node(data);
+
+        }
+        public void PrintList()
+        {
+            Node runner = head;
+            while (runner != null)
+            {
+                Console.Write(runner.data.ToString() + " ");
+                runner = runner.next;
+            }
+        }
+    }
+}
---
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Program.cs
@@ -0,0 +1,20 @@
+﻿using CsharpSolution.Linked_Lists;
+using System;
+
+namespace CsharpSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------1--------");
+            LinkedList list = new LinkedList();
+            list.Append(10);
+            list.Append(20);
+            list.Append(30);
+            list.Prepend(50);
+
+            list.PrintList();
+        }
+    }
+}
