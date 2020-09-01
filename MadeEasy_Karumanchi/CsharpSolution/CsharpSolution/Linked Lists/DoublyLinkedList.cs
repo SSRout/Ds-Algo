0e01effb38abae7e44f6b5673df31d700f98eb79
ssrout <smruti.sambit.rout@gmail.com>
2020-09-02 01:07:11 +0530

    double linked list added

---
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Linked Lists/DoublyLinkedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CsharpSolution.Linked_Lists
+{
+    /*
+     * Doubly Linked list with 
+     * Prepend
+     * Append
+     * InsertAfter
+     * InsertBefore
+     * Search Node
+     * Delete node
+     * Length of list
+     * Print List
+    */
+
+    #region Double Node
+    internal class DoubleNode<T>
+    {
+        internal T data;
+        internal DoubleNode<T> next;
+        internal DoubleNode<T> prev;
+        public DoubleNode(T data)
+        {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+    }
+    #endregion
+    class DoublyLinkedList<T>
+    {
+        DoubleNode<T> head;
+        public DoublyLinkedList()
+        {
+            head = null;
+        }
+
+        #region Insert At First
+        public void Prepend(T data)
+        {
+            DoubleNode<T> dnode = new DoubleNode<T>(data);
+            dnode.next = head;
+            dnode.prev = null;
+            if (head != null)
+            {
+                head.prev = dnode;
+            }
+            head = dnode;
+
+        }
+        #endregion
+
+        #region Insert At Last
+        public void Append(T data)
+        {
+            DoubleNode<T> temp = new DoubleNode<T>(data);
+            temp.next = null;
+            DoubleNode<T> current = head;
+            //if list is empty
+            if (current == null)
+            {
+                temp.prev = null;
+                head = temp;
+                return;
+            }
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = temp;
+            temp.prev = current;
+        }
+        #endregion
+
+        #region Print List
+        public void PrintList()
+        {
+            DoubleNode<T> node = head;
+            while (node!= null)
+            {
+                Console.Write(node.data + " ");
+                node = node.next;
+            }
+            Console.WriteLine();
+        }
+        #endregion
+
+    }
+}
--- MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Program.cs
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Program.cs
@@ -7,25 +7,61 @@ namespace CsharpSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------1.Single Linked List--------");
-            LinkedList<int> list = new LinkedList<int>();
-            for (int i = 1; i <= 5; i++)
-                list.Prepend(i);
-            list.InsertAfter(6, 5);
-            list.InsertBefore(7, 3);
-            list.PrintList();
-            Console.WriteLine($"\nLength of List before delete {list.NodeCount()}");
-            list.DelteNode(6);
-            list.PrintList();
-            Console.WriteLine($"\nLength of List after delete {list.NodeCount()}");
+            try
+            {
+            Menu:
+                Console.WriteLine("Press 0 For Exit.");
+                Console.WriteLine("Enter Question No: [1-100]\n");
+                int options = int.Parse(Console.ReadLine());
+                switch (options)
+                {
+                    case 0:
+                        Console.WriteLine("---Happy Learning See You Latter---");
+                        break;
+                    case 1:
 
-            LinkedList<string> list1 = new LinkedList<string>();
-            string []str= {"A","B","C","D","E"};
-            foreach (var v in str)
-                list1.Append(v);
-            list1.PrintList();
-            list1.SearchNode("D");
+                        Console.WriteLine("--------1.Single Linked List--------");
+                        LinkedList<int> list = new LinkedList<int>();
+                        for (int i = 1; i <= 5; i++)
+                            list.Prepend(i);
+                        list.InsertAfter(6, 5);
+                        list.InsertBefore(7, 3);
+                        list.PrintList();
+                        Console.WriteLine($"\nLength of List before delete {list.NodeCount()}");
+                        list.DelteNode(6);
+                        list.PrintList();
+                        Console.WriteLine($"\nLength of List after delete {list.NodeCount()}");
 
+                        LinkedList<string> list1 = new LinkedList<string>();
+                        string[] str = { "A", "B", "C", "D", "E" };
+                        foreach (var v in str)
+                            list1.Append(v);
+                        list1.PrintList();
+                        list1.SearchNode("D");
+                        goto Menu;
+
+
+                    case 2:
+                        Console.WriteLine("--------2.Doubly Linked List--------");
+                        DoublyLinkedList<int> dlist = new DoublyLinkedList<int>();
+                        dlist.Append(30);
+                        dlist.Append(40);
+                        dlist.Prepend(20);
+                        dlist.Prepend(10);
+                        dlist.PrintList();
+                        goto Menu;
+
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid Option Typed");
+                        goto Menu;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something Went Wrong\nCause: {ex.Message}");
+            }
         }
     }
 }
