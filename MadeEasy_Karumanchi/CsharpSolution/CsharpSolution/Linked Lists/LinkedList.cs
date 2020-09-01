3e72e536836c50bca652385a0fa656c59855159d
ssrout <smruti.sambit.rout@gmail.com>
2020-09-01 20:51:01 +0530

    single linkedlist

--- MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Linked Lists/LinkedList.cs
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Linked Lists/LinkedList.cs
@@ -1,69 +1,202 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CsharpSolution.Linked_Lists
 {
     /*
-     * Plain Linked list with 
+     * Single Linked list with 
      * Prepend
      * Append
-     * delete node
+     * InsertAfter
+     * InsertBefore
+     * Search Node
+     * Delete node
+     * Length of list
+     * Print List
     */
 
-    internal class Node
+
+    #region Single Node
+    internal class Node<T>
     {
-        internal int data;
-        internal Node next;
-        public Node(int d)
+        internal T data;
+        internal Node<T> next;
+        public Node(T data)
         {
             next = null;
-            data = d;
+            this.data = data;
         }
     }
-    public class LinkedList
+    #endregion
+
+
+    public class LinkedList<T>
     {
-        internal Node head;
-        int count;
+        internal Node<T> head;
         public LinkedList()
         {
             head = null;
-            count = 0;
         }
 
-        internal void Prepend(int data)
+        #region Insert At First
+        internal void Prepend(T data)
         {
-            Node newNode = new Node(data);
+            Node<T> newNode = new Node<T>(data);
             newNode.next = head;
             head = newNode;
-            count++;
         }
+        #endregion
 
-        internal void Append(int data)
+        #region Insert At Last
+        internal void Append(T data)
         {
             if (head == null)
             {
-                head = new Node(data);
-                count++;
+                head = new Node<T>(data);
+                //or
+                // Prepend(data);
                 return;
             }
-            Node currentNode = head;
+            Node<T> currentNode = head;
             while (currentNode.next != null)
             {
                 currentNode = currentNode.next;
             }
 
-            currentNode.next = new Node(data);
+            currentNode.next = new Node<T>(data);
 
         }
+        #endregion
+
+        #region Insert After Node
+        public void InsertAfter(T data, T key)
+        {
+            if (head == null) return;
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.data.Equals(key))
+                {
+                    Node<T> node = new Node<T>(data);
+                    node.next = current.next;
+                    current.next = node;
+                    return;
+                }
+                if (current.next == null)
+                {
+                    Console.WriteLine($"{key} Node is Not Found in List to insert new Node {data} After.");
+                    return;
+                }
+                current = current.next;
+            }
+        }
+        #endregion
+
+        #region Insert Before Node
+        public void InsertBefore(T data, T key)
+        {
+            if (head == null) return;
+
+            if (head.data.Equals(key))
+            {
+                Prepend(data);
+                return;
+            }
+            Node<T> current = head;
+            Node<T> previous = null;
+            while (current != null )
+            {
+                if (current.data.Equals(key))
+                {
+                    Node<T> node = new Node<T>(data);
+
+                    previous.next = node;
+                    node.next = current;
+                    return;
+                }
+                if (current.next == null)
+                {
+                    Console.WriteLine($"{key} Node is Not Found in List to insert new Node {data} Before.");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+        }
+        #endregion
+
+        #region Delete Node
+        public void DelteNode(T data)
+        {
+            if (head == null) return;
+            //delete head node
+            if (head.data.Equals(data))
+            {
+                head = head.next;
+                return;
+            }
+            Node<T> current = head;
+            while (current.next != null)
+            {
+                if (current.next.data.Equals(data))
+                {
+                    current.next = current.next.next;
+                    return;
+                }
+                
+                current = current.next;
+            }
+            if (current.next == null)
+            {
+                Console.WriteLine($"{data} Node is Not Found in List to delete.");
+                return;
+            }
+        }
+        #endregion
+
+        #region Search Node
+        public void SearchNode(T key)
+        {
+            int count = 1;
+            Node<T> cur = head;
+            while (cur != null)
+            {
+                if (cur.data.Equals(key))
+                {
+                    Console.WriteLine($"{key} found at index {count}");
+                    return;
+                }
+                count += 1;
+                cur = cur.next;
+            }
+        }
+        #endregion
+
+        #region List Length
+        public int NodeCount()
+        {
+            int count = 0;
+            Node<T> runner = head;
+            while (runner != null)
+            {
+                count += 1;
+                runner = runner.next;
+            }
+            return count;
+        }
+        #endregion
+
+        #region Print List
         public void PrintList()
         {
-            Node runner = head;
+            Node<T> runner = head;
             while (runner != null)
             {
                 Console.Write(runner.data.ToString() + " ");
                 runner = runner.next;
             }
+            Console.Write("\n");
         }
+        #endregion
+
     }
 }
--- MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Program.cs
+++ MadeEasy_Karumanchi/CsharpSolution/CsharpSolution/Program.cs
@@ -7,14 +7,25 @@ namespace CsharpSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------1--------");
-            LinkedList list = new LinkedList();
-            list.Append(10);
-            list.Append(20);
-            list.Append(30);
-            list.Prepend(50);
-
+            Console.WriteLine("--------1.Single Linked List--------");
+            LinkedList<int> list = new LinkedList<int>();
+            for (int i = 1; i <= 5; i++)
+                list.Prepend(i);
+            list.InsertAfter(6, 5);
+            list.InsertBefore(7, 3);
             list.PrintList();
+            Console.WriteLine($"\nLength of List before delete {list.NodeCount()}");
+            list.DelteNode(6);
+            list.PrintList();
+            Console.WriteLine($"\nLength of List after delete {list.NodeCount()}");
+
+            LinkedList<string> list1 = new LinkedList<string>();
+            string []str= {"A","B","C","D","E"};
+            foreach (var v in str)
+                list1.Append(v);
+            list1.PrintList();
+            list1.SearchNode("D");
+
         }
     }
 }
