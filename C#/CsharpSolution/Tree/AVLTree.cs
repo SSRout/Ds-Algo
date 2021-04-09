afa0b8268d342737538ffcba806e0cb53add773b
ssrout <smruti.sambit.rout@gmail.com>
2021-04-09 23:34:07 +0530

    QuckSort

---
+++ C#/CsharpSolution/Tree/AVLTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CsharpSolution.Tree
+{
+    public class AVLTree<T>
+    {
+        class Node<T>
+        {
+            public T data;
+            public Node<T> left;
+            public Node<T> right;
+            public Node<T> paent;
+            public Node(T obj)
+            {
+                data = obj;
+                paent = left = right = null;
+            }
+        }
+
+        private Node<T> root;
+        private int currentSize;
+
+        public AVLTree()
+        {
+            root = null;
+            currentSize = 0;
+        }
+
+        public void Add(T obj)
+        {
+            Node<T> node = new Node<T>(obj);
+            if (root == null)
+            {
+                root = node;
+                currentSize++;
+                return;
+            }
+            Add(root, node);
+        }
+
+
+        private void Add(Node<T> parent, Node<T> newNode)
+        {
+     
+        }
+    }
+}
