41469fc6c58b3a7149f66c230d0a4d6af2ea1135
Smruti Sambit Rout <smruti.sambit.rout@gmail.com>
2021-05-14 21:22:52 +0530

    Development (#13)
    * Selection Sort
    
    * t1
    
    * QuckSort
    
    * Merge Sort

---
+++ C#/CsharpSolution/Array and Strings/MergeSort.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpSolution.Array_and_Strings
+{
+    //O(nlogn)
+    public class MergeSort
+    {
+        public MergeSort(int[] arr)
+        {
+            Merge_Sort(arr, 0, arr.Length - 1);
+            printArray(arr);
+        }
+
+        void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        public void Merge_Sort(int[] arr,int l,int r)
+        {
+
+            if (l < r)
+            {
+                // Find the middle
+                // point
+                int m = l + (r - l) / 2;
+
+                // Sort first and
+                // second halves
+                Merge_Sort(arr, l, m);
+                Merge_Sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                Merging(arr, l, m, r);
+            }
+        }
+
+        private void Merging(int[] arr, int l, int m, int r)
+        {
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first
+            // and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarry array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of L[] if any
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
